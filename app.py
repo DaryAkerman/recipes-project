b7b0227cf79044af859bd08609da85f856e40584
DaryAkerman <daryakerman2002@gmail.com>
2024-06-07 19:32:13 +0300

    Made simple recipe site to add,update and delete recipes. Added docker compose file to run enviroment with 2 containers, 1 mongo 1 recipes-app.

--- app.py
+++ app.py
@@ -1,10 +1,45 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template, redirect, url_for
+from pymongo import MongoClient
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+# Connect to MongoDB
+client = MongoClient('mongodb://mongo:27017/')
+db = client.recipes_db
+recipes_collection = db.recipes
+
 @app.route('/')
-def hello_world():
-    return render_template('index.html')
+def index():
+    recipes = recipes_collection.find()
+    return render_template('index.html', recipes=recipes)
+
+@app.route('/add', methods=['POST'])
+def add_recipe():
+    recipe = {
+        'name': request.form['name'],
+        'ingredients': request.form['ingredients'],
+        'instructions': request.form['instructions']
+    }
+    recipes_collection.insert_one(recipe)
+    return redirect(url_for('index'))
+
+@app.route('/update/<id>', methods=['POST'])
+def update_recipe(id):
+    recipes_collection.update_one(
+        {'_id': ObjectId(id)},
+        {'$set': {
+            'name': request.form['name'],
+            'ingredients': request.form['ingredients'],
+            'instructions': request.form['instructions']
+        }}
+    )
+    return redirect(url_for('index'))
+
+@app.route('/delete/<id>', methods=['POST'])
+def delete_recipe(id):
+    recipes_collection.delete_one({'_id': ObjectId(id)})
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=3000, debug=True)
+    app.run(host='0.0.0.0', port=3000, debug=True)
